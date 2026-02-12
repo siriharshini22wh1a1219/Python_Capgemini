8741f6ed9bc37a9cc8f72da390c27d9fd9e25880
Chepuri Siri <chepurisiri@Chepuris-MacBook-Air.local>
2026-02-12 09:55:45 +0530

    assessment

---
+++ Banking_System.py
@@ -0,0 +1,38 @@
+class BankAccount:
+    
+    min_balance = 1000
+
+    def __init__(self, name, acc_no, balance):
+        self.name = name
+        self.acc_no = acc_no
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("deposited successfully.")
+        print("Updated Balance:",self.balance)
+
+    def withdraw(self, amount):
+        if self.balance - amount >= BankAccount.min_balance:
+            self.balance -= amount
+            print(f"{amount} withdrawn successfully.")
+            print(f"Remaining Balance: {self.balance}")
+        else:
+            print("Withdrawal denied! Minimum balance must be maintained.")
+   
+    def display_details(self):
+        print("Account Holder:", self.name)
+        print("Account Number:", self.acc_no)
+        print("Balance:", self.balance)
+        print("Minimum Balance:", BankAccount.min_balance)
+   
+    @classmethod
+    def update_min_balance(cls, new_balance):
+        cls.min_balance = new_balance
+        print("Minimum balance updated to:", cls.min_balance)
+
+acc1 = BankAccount("Harshini", 12345, 5000)
+acc1.deposit(2000)
+acc1.withdraw(3000)
+acc1.display_details()
+BankAccount.update_min_balance(2000)
